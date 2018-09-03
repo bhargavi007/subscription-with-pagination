b9e936c1b8660b2c102b2e65ff42272d83a58415
Bhargavi Addagulla <bhargavi@crowdspots.com>
2018-09-03 16:48:22 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,11 @@
+Package.describe({
+  name: 'siliconangle:paginated-subscription',
+  summary: "Easily paginate a subscription that takes a limit parameter.",
+  version: "0.2.4",
+  git: "https://github.com/siliconangle/paginated_subscription"
+});
+
+Package.onUse(function (api, where) {
+  api.versionsFrom("METEOR@1.3.0");
+  api.addFiles('paginated_subscription.js', 'client');
+});

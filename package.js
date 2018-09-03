7b2c8b699aa3027b3e9ed5a59cfe31daafb6e0e7
Bhargavi Addagulla <bhargavi@crowdspots.com>
2018-09-03 16:59:25 +0530

    Update package.js

--- package.js
+++ package.js
@@ -2,6 +2,7 @@ Package.describe({
   name: 'siliconangle:paginated-subscription',
   summary: "Easily paginate a subscription that takes a limit parameter.",
   version: "0.1.0",
+  documentation : "https://github.com/siliconangle/paginated_subscription/blob/master/README.md",
   git: "https://github.com/siliconangle/paginated_subscription"
 });
 
